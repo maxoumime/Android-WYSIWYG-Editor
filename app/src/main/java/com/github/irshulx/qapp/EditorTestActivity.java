03dc7b4b3a0f8c4fbb68fe94bfe9cc3a1bf15811
Maxime Bertheau <maxime@frontapp.com>
2016-11-04 13:00:45 -0700

    Lib rework

--- app/src/main/java/com/github/irshulx/qapp/EditorTestActivity.java
+++ app/src/main/java/com/github/irshulx/qapp/EditorTestActivity.java
@@ -17,133 +17,30 @@ import android.widget.EditText;
 import android.widget.Toast;
 import com.github.irshulx.Editor;
 import com.github.irshulx.EditorCore;
+import com.github.irshulx.EditorToolbar;
 import com.github.irshulx.models.EditorTextStyle;
 
 import java.io.IOException;
 
 public class EditorTestActivity extends AppCompatActivity {
     Editor _editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor_test);
         _editor= (Editor) findViewById(R.id.editor);
-        CreateEditor();
-    }
-    private void CreateEditor() {
-        findViewById(R.id.action_h1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.UpdateTextStyle(EditorTextStyle.H1);
-            }
-        });
 
-        findViewById(R.id.action_h2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.UpdateTextStyle(EditorTextStyle.H2);
-            }
-        });
+        _editor.setOptions(
+                EditorToolbar.TOOLBAR_OPTION.BOLD,
+                EditorToolbar.TOOLBAR_OPTION.ITALIC,
+                EditorToolbar.TOOLBAR_OPTION.HR,
+                EditorToolbar.TOOLBAR_OPTION.UNORDERED_NUMBERED,
+                EditorToolbar.TOOLBAR_OPTION.BULLETED
+            );
 
-        findViewById(R.id.action_h3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.UpdateTextStyle(EditorTextStyle.H3);
-            }
-        });
+        _editor.Render();
 
-        findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.UpdateTextStyle(EditorTextStyle.BOLD);
-            }
-        });
-
-        findViewById(R.id.action_Italic).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.UpdateTextStyle(EditorTextStyle.ITALIC);
-            }
-        });
-
-        findViewById(R.id.action_indent).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.UpdateTextStyle(EditorTextStyle.INDENT);
-            }
-        });
-
-        findViewById(R.id.action_outdent).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.UpdateTextStyle(EditorTextStyle.OUTDENT);
-            }
-        });
-
-        findViewById(R.id.action_bulleted).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.InsertList(false);
-            }
-        });
-
-        findViewById(R.id.action_unordered_numbered).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.InsertList(true);
-            }
-        });
-
-        findViewById(R.id.action_hr).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.InsertDivider();
-            }
-        });
-
-        findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.OpenImagePicker();
-            }
-        });
-
-        findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.InsertLink();
-            }
-        });
-
-        findViewById(R.id.action_map).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.InsertMap();
-            }
-        });
-
-        findViewById(R.id.action_erase).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _editor.clearAllContents();
-            }
-        });
-
-        //  _editor.dividerBackground=R.drawable.divider_background_dark;
-        _editor.setImageUploaderUri("http://192.168.43.239/Laser-Editor-WebApi/api/ImageUploaderApi/PostImage");
-        _editor.setFontFace(R.string.fontFamily__serif);
-        _editor.setDividerLayout(R.layout.tmpl_divider_layout);
-        _editor.setEditorImageLayout(R.layout.tmpl_image_view);
-        _editor.setListItemLayout(R.layout.tmpl_list_item);
-        //_editor.StartEditor();
-        _editor.setEditorListener(new EditorCore.EditorListener() {
-            @Override
-            public void onTextChanged(EditText editText, Editable text) {
-                // Toast.makeText(EditorTestActivity.this, text, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        _editor.Render();  // this method must be called to start the editor
 
         findViewById(R.id.btnRender).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -166,36 +63,6 @@ public class EditorTestActivity extends AppCompatActivity {
         background.setStroke(4, Color.WHITE);
         background.setCornerRadius(radius);
         button.setBackgroundDrawable(background);
-    }
-
-    private void Render() {
-        String x="<h2 id=\"installation\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;color:#c00; margin-top: -80px !important;\">Installation</h2>"+
-                "<h3 id=\"requires-html5-doctype\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin-bottom: 8px; margin-right: 0px; margin-left: 0px;\">Requires HTML5 doctype</h3>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\">Bootstrap uses certain HTML elements and CSS properties which require HTML5 doctype. Include&nbsp;<code style=\"font-size: 12.6px;\">&lt;!DOCTYPE html&gt;</code>&nbsp;in the beginning of all your projects.</p>"+
-                "<img src=\"http://www.scifibloggers.com/wp-content/uploads/TOR_2.jpg\" />"+
-                "<h2 id=\"integration\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin-top: -80px !important;\">Integration</h2>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\">3rd parties available in django, rails, angular and so on.</p>"+
-                "<h3 id=\"django\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin-bottom: 8px; margin-right: 0px; margin-left: 0px;\">Django</h3>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\">Handy update for your django admin page.</p>"+
-                "<ul style=\"color: rgb(51, 51, 51);\"><li style=\"font-size: 14px; color: rgb(104, 116, 127);\"><a href=\"https://github.com/summernote/django-summernote\" target=\"_blank\">django-summernote</a></li><li style=\"font-size: 14px; color: rgb(104, 116, 127);\"><a href=\"https://pypi.python.org/pypi/django-summernote\" target=\"_blank\">summernote plugin for Django</a></li></ul>"+
-                "<h3 id=\"ruby-on-rails\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin-bottom: 8px; margin-right: 0px; margin-left: 0px;\">Ruby On Rails</h3>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\">This gem was built to gemify the assets used in Summernote.</p>"+
-                "<ul style=\"color: rgb(51, 51, 51);\"><li style=\"font-size: 14px; color: rgb(104, 116, 127);\"><a href=\"https://github.com/summernote/summernote-rails\" target=\"_blank\">summernote-rails</a></li></ul>"+
-                "<h3 id=\"angularjs\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin-bottom: 8px; margin-right: 0px; margin-left: 0px;\">AngularJS</h3>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\">AngularJS directive to Summernote.</p>"+
-                "<ul style=\"color: rgb(51, 51, 51);\"><li style=\"font-size: 14px; color: rgb(104, 116, 127);\"><a href=\"https://github.com/summernote/angular-summernote\">angular-summernote</a></li></ul>"+
-                "<h3 id=\"apache-wicket\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin-bottom: 8px; margin-right: 0px; margin-left: 0px;\">Apache Wicket</h3>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\">Summernote widget for Wicket Bootstrap.</p>"+
-                "<ul style=\"color: rgb(51, 51, 51);\"><li style=\"font-size: 14px; color: rgb(104, 116, 127);\"><a href=\"http://wb-mgrigorov.rhcloud.com/summernote\" target=\"_blank\">demo</a></li><li style=\"font-size: 14px; color: rgb(104, 116, 127);\"><a href=\"https://github.com/l0rdn1kk0n/wicket-bootstrap/tree/4f97ca783f7279ca43f9e2ee790703161f59fa40/bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/editor\" target=\"_blank\">source code</a></li></ul>"+
-                "<h3 id=\"webpack\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin-bottom: 8px; margin-right: 0px; margin-left: 0px;\">Webpack</h3>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\">Example about using summernote with webpack.</p>"+
-                "<ul style=\"color: rgb(51, 51, 51);\"><li style=\"font-size: 14px; color: rgb(104, 116, 127);\"><a href=\"https://github.com/hackerwins/summernote-webpack-example\" target=\"_blank\">summernote-webpack-example</a></li></ul>"+
-                "<h3 id=\"meteor\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin-bottom: 8px; margin-right: 0px; margin-left: 0px;\">Meteor</h3>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\">Example about using summernote with meteor.</p>"+
-                "<ul style=\"color: rgb(51, 51, 51);\"><li style=\"font-size: 14px; color: rgb(104, 116, 127);\"><a href=\"https://github.com/hackerwins/summernote-meteor-example\" target=\"_blank\">summernote-meteor-example</a></li></ul>"+
-                "<p style=\"font-size: 14px; color: rgb(104, 116, 127);\"><br></p>";
-        _editor.Render();
-
     }
 
     @Override
